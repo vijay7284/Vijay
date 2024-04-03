785bfc824627c4c3ef464a0147104d7bb62ba8f2
vijay7284 <90247305+vijay7284@users.noreply.github.com>
2024-04-03 19:04:46 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-2"  # Choose your desired region
 }
 
-resource "aws_instance" "my_ec2_instance" {
+resource "aws_instance" "my_ecinstance" {
   ami           = "ami-0944e91aed79c721c"  # Amazon Linux 2 AMI ID
   instance_type = "t2.micro"
   //key_name      = "my-keypair"  # Replace with your SSH key pair name
